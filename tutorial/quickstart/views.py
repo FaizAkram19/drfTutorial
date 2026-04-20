16fdd3f493344df0057352c3fcb9b72b911e48f5
Faiz Akram <172576315+FaizAkram19@users.noreply.github.com>
2026-04-20 15:52:15 +0000

    Initial Commit

---
+++ tutorial/quickstart/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.contrib.auth.models import User, Group
+from rest_framework import permissions, viewsets
+from tutorial.quickstart.serializers import UserSerializer, GroupSerializer
